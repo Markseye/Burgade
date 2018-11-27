56ddf61dd8088df591e7463b64b8b408cb95a1db
Mark Ioannidis <mark.ioannidis@gmail.com>
2018-11-27 00:58:14 -0500

    Updates

---
+++ precache-manifest.49794b763f64a1bcdae878319132c046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2551c439e02d4cb1f5dc5e3bf43efe6",
+    "url": "/static/media/burger.b2551c43.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c1bf9af0aacf72763fd",
+    "url": "/static/js/main.6c1bf9af.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "6c1bf9af0aacf72763fd",
+    "url": "/static/css/main.b20f34e2.chunk.css"
+  },
+  {
+    "revision": "6548a74f81c6e6a7a0aab6cbf621da10",
+    "url": "/index.html"
+  }
+];
